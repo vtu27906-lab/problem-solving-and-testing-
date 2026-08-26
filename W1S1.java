138a1404d4064149c9b44b1700d7672b1ab3abb5
sameera <vtu27906@veltech.edu.in>
2026-08-26 18:55:18 +0530

    Add files via upload

---
+++ W1S1.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    interface PerformOperation {
+        boolean check(int a);
+    }
+
+    public static PerformOperation isOdd() {
+        return n -> n % 2 != 0;
+    }
+
+    public static PerformOperation isPrime() {
+        return n -> {
+            if (n < 2)
+                return false;
+
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        };
+    }
+
+    public static PerformOperation isPalindrome() {
+        return n -> {
+            int original = n;
+            int reverse = 0;
+
+            while (n > 0) {
+                reverse = reverse * 10 + n % 10;
+                n /= 10;
+            }
+
+            return original == reverse;
+        };
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+
+            int condition = sc.nextInt();
+            int num = sc.nextInt();
+
+            boolean result;
+
+            if (condition == 1) {
+                result = isOdd().check(num);
+
+                if (result)
+                    System.out.println("ODD");
+                else
+                    System.out.println("EVEN");
+
+            } else if (condition == 2) {
+                result = isPrime().check(num);
+
+                if (result)
+                    System.out.println("PRIME");
+                else
+                    System.out.println("COMPOSITE");
+
+            } else {
+                result = isPalindrome().check(num);
+
+                if (result)
+                    System.out.println("PALINDROME");
+                else
+                    System.out.println("NOT PALINDROME");
+            }
+        }
+
+        sc.close();
+    }
+}
+
+    
+    
